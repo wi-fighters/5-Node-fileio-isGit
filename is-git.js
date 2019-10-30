627c8184dbe4c967ca7da3daba29a573291a57ee
Itamar Givon <itamar.givon.dev@gmail.com>
2019-10-30 11:14:10 +0100

    Solve task 1

--- is-git.js
+++ is-git.js
@@ -2,4 +2,14 @@ const path = require('path');
 const dirExists = require('directory-exists');
 
 module.exports = (relativePath) => {
+    const absolutePath = path.resolve(relativePath);
+
+    if(!dirExists.sync(absolutePath)) {
+        console.error(`Sorry, but I couldn't find a directory at ${absolutePath}`);
+        process.exit();
+    }
+
+    const gitPath = path.join(absolutePath, '.git');
+
+    return dirExists.sync(gitPath) ? `${absolutePath} is a git repository` : `${absolutePath} is **not** a git repository`;
 }
